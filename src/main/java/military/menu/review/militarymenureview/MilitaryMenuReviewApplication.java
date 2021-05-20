34d3c56b0b33e1cff1387c3173e92c913939f5f1
wilgur513 <wilgur513@naver.com>
2021-05-20 23:18:31 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/military/menu/review/militarymenureview/MilitaryMenuReviewApplication.java
@@ -0,0 +1,12 @@
+package military.menu.review.militarymenureview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitaryMenuReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilitaryMenuReviewApplication.class, args);
+	}
+}
---
+++ src/test/java/military/menu/review/militarymenureview/MilitaryMenuReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package military.menu.review.militarymenureview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryMenuReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
