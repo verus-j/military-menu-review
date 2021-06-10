ea6920c53a875c9313a1a1da2dcd9260958be5bb
wilgur513 <wilgur513@naver.com>
2021-06-10 11:22:07 +0900

    MemberRepository 생성

---
+++ src/main/java/military/menu/review/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package military.menu.review.repository;
+
+import military.menu.review.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
