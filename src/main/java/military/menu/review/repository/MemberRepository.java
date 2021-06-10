129325e0969b3c3e0ada4e4dd7a746d208e78186
wilgur513 <wilgur513@naver.com>
2021-06-10 11:30:15 +0900

    MemberRepository에서 findByUsername 정의

--- src/main/java/military/menu/review/repository/MemberRepository.java
+++ src/main/java/military/menu/review/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import military.menu.review.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByUsername(String username);
+
 }
