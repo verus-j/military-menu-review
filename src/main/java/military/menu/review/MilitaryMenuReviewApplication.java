4ff76e99519eca539b149e7154838be0cc720f6e
wilgur513 <wilgur513@naver.com>
2021-06-10 22:26:36 +0900

    DailyMealController에서 Map으로 변환 후 반환

--- src/main/java/military/menu/review/MilitaryMenuReviewApplication.java
+++ src/main/java/military/menu/review/MilitaryMenuReviewApplication.java
@@ -1,22 +1,49 @@
 package military.menu.review;
 
+import lombok.RequiredArgsConstructor;
+import military.menu.review.domain.Like;
+import military.menu.review.domain.Member;
+import military.menu.review.domain.Menu;
+import military.menu.review.domain.Role;
+import military.menu.review.repository.MemberRepository;
+import military.menu.review.service.LikeService;
+import military.menu.review.service.MemberService;
+import military.menu.review.service.MenuService;
 import military.menu.review.service.mnd.MndService;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class MilitaryMenuReviewApplication {
+	private final MemberService memberService;
+	private final MenuService menuService;
+	private final LikeService likeService;
 	public static void main(String[] args) {
 		SpringApplication.run(MilitaryMenuReviewApplication.class, args);
 	}
 
 	@Bean
 	public ApplicationRunner runner(MndService service) {
-		return (args) -> {
-			service.saveFromApi();
+		return new ApplicationRunner() {
+			@Override
+			@Transactional
+			public void run(ApplicationArguments args) throws Exception {
+				service.saveFromApi();
+				Member member = Member.of("user", "123", "kim", "11-11111", Role.SOLDIER);
+				Member normal = Member.of("normal", "123", "lee", "11-123211", Role.NORMAL);
+				memberService.join(member);
+				memberService.join(normal);
+				Menu menu = menuService.findByName("밥");
+				likeService.like(member, menu);
+			}
 		};
 	}
 }
--- src/main/java/military/menu/review/controller/DailyMealController.java
+++ src/main/java/military/menu/review/controller/DailyMealController.java
@@ -1,11 +1,11 @@
 package military.menu.review.controller;
 
 import lombok.RequiredArgsConstructor;
+import military.menu.review.security.MemberContext;
+import military.menu.review.service.MenuService;
 import military.menu.review.service.dto.DailyMealDTO;
 import military.menu.review.service.mnd.api.MndApi;
-import military.menu.review.service.mnd.api.parser.DailyMealsParser;
 import military.menu.review.service.DailyMealService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/dailyMeal")
@@ -21,29 +22,25 @@ import java.util.List;
 public class DailyMealController {
     private final MndApi api;
     private final DailyMealService dailyMealService;
+    private final MenuService menuService;
+    private final MemberContext context;
 
     @GetMapping("/one")
-    public ResponseEntity<DailyMealDTO> dailyMeal(@RequestParam String date) {
-        return new ResponseEntity<>(
-                dailyMealService.findByDate(toDate(date)),
-                HttpStatus.OK
-        );
+    public ResponseEntity<Map<String, Object>> dailyMeal(@RequestParam String date) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("meal", dailyMealService.findByDate(toDate(date)));
+        map.put("liked", menuService.findByMemberLikedAndDate(context, toDate(date)));
+
+        return ResponseEntity.ok(map);
     }
 
     @GetMapping("/list")
-    public ResponseEntity<List<DailyMealDTO>> list(@RequestParam String start, @RequestParam String end) {
-        return new ResponseEntity(
-                dailyMealService.findByDateBetween(toDate(start), toDate(end)),
-                HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<List<DailyMealDTO>> test() {
-        return new ResponseEntity(
-                api.parse(new DailyMealsParser()),
-                HttpStatus.OK
-        );
+    public ResponseEntity<Map<String, Object>> list(@RequestParam String start, @RequestParam String end) {
+        System.out.println(context.getCurrentMember());
+        Map<String, Object> map = new HashMap<>();
+        map.put("meal", dailyMealService.findByDateBetween(toDate(start), toDate(end)));
+        map.put("liked", menuService.findByMemberLikedAndDateBetween(context, toDate(start), toDate(end)));
+        return ResponseEntity.ok(map);
     }
 
     private LocalDate toDate(String date) {
