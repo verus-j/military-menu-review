97f8754afdabafee4482e02d590f1029e3b69c72
wilgur513 <wilgur513@naver.com>
2021-06-24 18:33:46 +0900

    MealMenuService 구현

--- src/main/java/military/menu/review/repository/MealMenuRepository.java
+++ src/main/java/military/menu/review/repository/MealMenuRepository.java
@@ -2,8 +2,15 @@ package military.menu.review.repository;
 
 import military.menu.review.domain.MealMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface MealMenuRepository extends JpaRepository<MealMenu, Long> {
+    @Query("select mm from MealMenu mm join fetch mm.meal join fetch mm.menu join mm.meal ma where ma.date between :start and :end")
+    List<MealMenu> findByDateBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
